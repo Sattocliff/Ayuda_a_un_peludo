edc0279b1467711e908799bffd9e527d11b51942
Sattocliff <her.gonzalez.andres@gmail.com>
2023-03-28 22:24:46 -0300

    commit definitivo

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function alerta()  {
+  Swal.fire({
+    title: 'Gracias por demostrar tu apoyo!',
+    text: 'Es muy importante para nosotros conocer el amor de ustedes!',
+    imageUrl: 'img/catdance.gif',
+    background: 'pink'
+      })
+}
+
+function alertita(){
+  Swal.fire({
+    title: 'Su formulario ha sido enviado',
+    text: 'Nos contactaremos pronto',
+    icon: 'success'
+      })
+}
